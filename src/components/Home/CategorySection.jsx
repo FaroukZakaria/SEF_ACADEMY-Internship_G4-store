f2b909b9931aba2605d6a4b9ead46a634e21e034
Pierre Ehab <108834103+PierreEhab-1337@users.noreply.github.com>
2026-07-29 16:40:27 +0400

    Edits (#11)
    * Minor Edits
    
    * refactor: remove unused imports from App component
    
    * fix: update CartItem quantity text color for better visibility
    
    * fix: remove console log for token in MyOrders component
    
    * fix: solved search bar issue and removed fixed width
    
    ---------
    
    Co-authored-by: ehab <ehab@DESKTOP-6K3F9V9>
    Co-authored-by: FaroukZakaria <faroukzakaria2003@gmail.com>

--- src/components/Home/CategorySection.jsx
+++ src/components/Home/CategorySection.jsx
@@ -2,10 +2,10 @@ const CategorySection = ({categories, loadingProducts}) => {
     return(
         <section id="categories" className="bg-amazon-surface w-full">
         <div className="bg-amazon-bg/30 w-full flex justify-center text-center pb-10 px-6 lg:px-8">
-          <div>
+          <div className="w-full">
             <p className="font-bold text-3xl mt-18 mb-2">Shop by Category</p>
             <p className="text-amazon-textLight mb-15">Browse our wide range of categories</p>
-            <div className="grid grid-cols-2 sm:grid-cols-3 lg:grid-cols-4 gap-6 mt-7">
+            <div className="grid grid-cols-2 sm:grid-cols-3 lg:grid-cols-4 gap-6 mt-7 w-full">
               {loadingProducts ?
                 (
                   //Categories Skeleton
@@ -22,7 +22,7 @@ const CategorySection = ({categories, loadingProducts}) => {
                 ) : (
                   categories.map((category, index) => (
                     category.count ? 
-                      <a key={index} className="hover:shadow-md group p-5 w-[220px] lg:w-[280px] rounded-2xl bg-amazon-surface border border-amazon-border hover:border-amazon-orange flex flex-col items-center"
+                      <a key={index} className="hover:shadow-md group p-5 w-full min-w-[200px] rounded-2xl bg-amazon-surface border border-amazon-border hover:border-amazon-orange flex flex-col items-center"
                         href={"/shop?category=" + category.value}>
                         <div className="mb-6 bg-amazon-orange/20 group-hover:bg-amazon-orange/30 rounded-lg p-3">{category.icon}</div>
                         <div className="font-medium">{category.title}</div>
